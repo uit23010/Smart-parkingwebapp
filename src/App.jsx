77794a84320be2630c8d50cb3d163d5f35865f78
uit23010 <230205.it@rmkec.ac.in>
2025-04-01 19:01:32 +0530

    Modified App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"; 
 import HomePage from "./components/Homepage";
-import LoginPage from "./components/Loginpage";
+import Loginpage from "./components/LoginPage";
 import SignupPage from "./components/Signuppage";
 import Dashboard from "./components/Dashboard";
 import SmartParkingPage from "./components/SmartParkingBooking";
@@ -11,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<Loginpage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/smart-parking" element={<SmartParkingPage />} />
